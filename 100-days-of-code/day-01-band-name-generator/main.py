31deca87be5c5a80cc93abcd97d9191abc9e8cec
Aftab009Khan <sonuaftabkhan9211@gmail.com>
2026-05-24 02:12:41 +0530

    Create main.py

---
+++ 100-days-of-code/day-01-band-name-generator/main.py
@@ -0,0 +1,17 @@
+# ==========================================
+# DAY 1 PROJECT: BAND NAME GENERATOR
+# ==========================================
+
+print("========================================")
+print(" WELCOME TO THE BAND NAME GENERATOR! ")
+print("========================================")
+
+# Step 1: Ask the user for the city name
+ 
+city = input("What is the name of the city you grew up in?\n")
+
+# Step 2: Ask the user for the name of a pet
+pet = input("What is the name of your favorite pet?\n")
+
+# Step 3: Display the final result to the user
+print(" Your awesome band name could be: " + band_name )
