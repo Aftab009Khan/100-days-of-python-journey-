9ba78e7c5e74bb5cb734eaa4d0441dadc20c0d7e
Aftab009Khan <sonuaftabkhan9211@gmail.com>
2026-05-24 01:47:57 +0530

    Update main.py

--- project/Day_01_band_name_generator/main.py
+++ project/Day_01_band_name_generator/main.py
@@ -1,1 +1,12 @@
+#band name generator 
+print("===================================")
+print("  BAND NAME GENERATOR 2026  ")
+print("===================================")
+print("Let's find your rockstar name!\n")
 
+# 2. Ask the user for their city and pet name
+city = input("What city did you grow up in?\n> ")
+pet = input("What is the name of your pet?\n> ")
+
+# 3.display band name 
+print("\n🔥 Your band name is: " + city + " " + pet)
